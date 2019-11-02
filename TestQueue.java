4568d62629a585c15b3600e225c1e9e3fc208dfd
liu <1373788543@qq.com>
2019-11-02 16:47:10 +0800

    queue & stack

--- TestQueue.java
+++ TestQueue.java
@@ -1,6 +1,6 @@
 package Queue;
 
-public class Test {
+public class TestQueue {
     public static void main(String[] args) {
         MyQueue queue = new MyQueue();
         queue.offer(1);
@@ -8,10 +8,14 @@ public class Test {
         queue.offer(3);
         queue.offer(4);
 
+        System.out.println("队列长度： " + queue.size());
+
         while(!queue.isEmpty()){
             int curFront = queue.peek();
             System.out.print(curFront + " ");
             queue.poll();
         }
+
+
     }
 }
